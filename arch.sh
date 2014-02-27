1df29ace46f349174e37d67a1a3a739c70c293fe
Huihuo Zheng <zhenghh04@gmail.com>
2014-02-27 13:16:33 -0600

    Addubg arch.sh

---
+++ arch.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+WORKDIR=$HOME/Documents/Research/vo2/Programs/qwalk-zhh
+function compileconverter() {
+    cd $WORKDIR/src/converter
+    make 
+    cd -
+}
+
+function runqwalk()
+{
+    if [[ $# < 2 ]] 
+    then
+	$WORKDIR/src/qwalk-Linux-mpi $1
+	echo "$WORKDIR/src/qwalk-Linux-mpi $1"
+    else
+	echo "mpirun -np $2 $WORKDIR/src/qwalk-Linux-mpi $1"
+	mpirun -np $2 $WORKDIR/src/qwalk-Linux-mpi $1
+    fi
+}
+
+function runqwalk_old()
+{
+    if [[ $# < 2 ]] 
+    then
+	qwalk $1
+    else
+	mpirun -np $2 qwalk $1
+    fi
+}
+
+
+function rungosling()
+{
+    $WORKDIR/src/gosling-Linux-mpi $@
+}
+
+function runcrystal2qmc() {
+    $WORKDIR/src/converter/crystal2qmc-Linux $@
+}
+
+function runcrystal_post() {
+    sed -f ~/Templates/crystalout.sed $@
+}
