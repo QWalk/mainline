9e8290d1f799b0cbdd319a6d5d724c06330142f5
Huihuo Zheng <zhenghh04@gmail.com>
2014-05-09 01:36:18 -0500

    corrected bug for newk

--- utils/newk.sh
+++ utils/newk.sh
@@ -3,5 +3,6 @@ if [ $# -lt 3 ]; then
     echo "Syntax: newk.sh crystal_out newk_out output"
     exit 1
 fi
-sed -n -e '1,/FINAL EIGENVECTORS/p' -e 's/FINAL EIGENVECTORS/HAMILTONIAN EIGENVECTORS/g' $1 > $3
+head -50000 $1 > $1.tmp
+sed -n -e '1,/FINAL EIGENVECTORS/p' $1.tmp | sed 's/FINAL EIGENVECTORS/NEWK EIGENVECTORS/g' > $3
 sed -n -e '/HAMILTONIAN EIGENVECTORS/, $p' $2 >> $3
