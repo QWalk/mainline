f4a2274411b11854da92105de4c26955142f8e4e
Lucas Wagner <lucas.wagner@gmail.com>
2014-12-27 13:03:13 -0600

    Revert the reversion!!

--- src/system/Pseudopotential.cpp
+++ src/system/Pseudopotential.cpp
@@ -568,8 +568,8 @@ void Pseudopotential::calcNonlocWithAllvariables(Wavefunction_data * wfdata,
               }
               else { 
                 Tmove nwtmove; nwtmove.pos.Resize(3);
-                sample->getElectronPos(e,nwtmove.pos);
-                //nwtmove.pos=newpos;
+                //sample->getElectronPos(e,nwtmove.pos);
+                nwtmove.pos=newpos;
                 nwtmove.e=e;
                 nwtmove.vxx=vxx;
                 tmoves.push_back(nwtmove);
