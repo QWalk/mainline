14cb1e875669a7d03bdda540b2b3579600003173
Joshua Schiller <joshuaSchiller@vpnpool-130-126-152-233.near.illinois.edu>
2015-08-13 21:59:41 -0500

    Added files

---
+++ utils/autogen/Specific/run_dmc_taub.py
@@ -0,0 +1,41 @@
+# QWalkRunDMC for Taub
+
+def execute(px_ssh, infile, outfile):
+	qscript = """#PBS -l nodes=1:ppn=12
+#PBS -l walltime=04:00:00
+#PBS -j oe
+#PBS -q secondary
+#PBS -o QSUB.stdout
+
+cd ${PBS_O_WORKDIR}
+module unload openmpi/1.6.4-intel-13.1
+module load openmpi/1.4-intel
+mpirun ~/bin/qwalk %s >& %s"""%(infile, outfile)
+	
+	px_ssh.sendline("echo '" + qscript + "' > batch_script")
+	px_ssh.prompt()
+	px_ssh.sendline('qsub batch_script')
+	px_ssh.prompt()
+	return px_ssh.before.strip().split('\r\n')[1].split('.')[0]
+
+def status(px_ssh, queue_id):
+	px_ssh.sendline("qstat | grep " + str(queue_id))
+	px_ssh.prompt()
+
+	try:
+		stat = px_ssh.before.strip().split('\r\n')[1].split()[4]
+	except:
+		stat = ''
+
+	if stat == 'R' or stat == 'Q':
+		return 'running'
+	else:
+		return 'not_started'
+
+def cancel(px_ssh, queue_id):
+	px_ssh.sendline("qstat")
+	px_ssh.prompt()
+
+	if queue_id in px_ssh.before:
+		px_ssh.sendline("qdel " + str(queue_id))
+		px_ssh.prompt()	
---
+++ utils/autogen/submission_tools.py
@@ -0,0 +1,69 @@
+import subprocess
+import importlib
+import os
+import shutil
+import sys
+import time
+
+class RemoteSubmitter:
+	def __init__(self, px_ssh, px_ftp, module, remotePath):
+		sys.path.append(os.getcwd() + '/Specific/')
+		self.px_ssh = px_ssh
+		self.px_ftp = px_ftp
+		self.remotePath = remotePath
+		self.module = importlib.import_module(module)
+
+	def execute(self, job_record, infiles, runfile, outfile):
+		job_id = str(job_record['control']['id'])
+		self.px_ssh.sendline('mkdir -p ' + self.remotePath + job_id)
+		self.px_ssh.prompt()
+		self.px_ssh.sendline('cd ' + self.remotePath + job_id)
+		self.px_ssh.prompt()
+
+		self.px_ssh.sendline('ls')
+		self.px_ssh.prompt()
+		for dep in infiles:		
+			self.px_ftp.sendline('put ' + os.getcwd() + '/' + dep + ' ' + self.remotePath + job_id)
+			self.px_ftp.expect('sftp>')
+
+		job_record['control']['queue_id'] = self.module.execute(self.px_ssh, runfile, outfile)
+		return job_record['control']['queue_id']
+
+	def status(self, job_record):
+		job_id = str(job_record['control']['id'])
+		self.px_ssh.sendline('cd ' + self.remotePath + job_id)
+		self.px_ssh.prompt()
+		return self.module.status(self.px_ssh, job_record['control']['queue_id'])
+
+	def output(self, job_record, outfiles):
+		job_id = str(job_record['control']['id'])
+		for cop in outfiles:
+			self.px_ssh.sendline('cd ' + self.remotePath + job_id)
+			self.px_ssh.prompt()
+			self.px_ssh.sendline('ls')
+			self.px_ssh.prompt()
+			remote_files = self.px_ssh.before
+			if cop in remote_files:
+				self.px_ftp.sendline('get ' + self.remotePath + job_id + '/' + cop + ' ' + os.getcwd())
+				self.px_ftp.expect('sftp>')
+			
+	def cancel(self, queue_id):
+		output = self.module.cancel(self.px_ssh, queue_id)
+
+class LocalSubmitter:
+	def __init__(self, module):
+		sys.path.append(os.getcwd() + '/Specific/')
+		self.module = importlib.import_module(module)
+
+	def execute(self,job_record,infiles,runfile, outfile):
+		job_record['control']['queue_id'] = self.module.execute(runfile, outfile)
+
+	def status(self,job_record):
+		return self.module.status(job_record['control']['queue_id'])
+
+	def output(self,job_record,outfiles):
+		pass
+
+	def cancel(self, queue_id):
+		output = self.module.cancel(queue_id)
+
