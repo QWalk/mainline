4f64a571983a0217ff65d578a3040434946ec0e2
Brian Busemeyer <bbusemeyer@gmail.com>
2015-12-19 17:03:21 -0600

    Clarified issue with job_prep

--- utils/autogen/job_prep.py
+++ utils/autogen/job_prep.py
@@ -6,7 +6,16 @@ def move_atom(input_cif, output_cif, atom_idx, displacement_vec):
   Reads input cif (as file name), moves atom by displacement vector (in
   angstroms), and writes to output_cif (as file name).
 
-  Warning: Currently seems to drop occupations information.
+  Currently, doesn't take into account the symmetry operations. This is
+  theoretically possible with PyMatGen, although I found it to be buggy so
+  decided it might not be worth it. 
+  Useful things to look up for implementing this:
+
+  pymatgen.symmetry.analyzer.SpacegroupAnalyzer.get_symmetrized_structure
+
+  and its resulting method
+
+  find_equivalent_sites()
   """
   parser    = CifParser(input_cif)
   struct    = parser.get_structures()[0]
