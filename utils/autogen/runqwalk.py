d353c1c29bcb15679617b079b565aaf582bbcffa
Will Wheeler <willwheelera@gmail.com>
2016-03-24 13:53:43 -0500

    Undoing changes in RunMaximize

--- utils/autogen/runqwalk.py
+++ utils/autogen/runqwalk.py
@@ -582,7 +582,7 @@ class QWalkRunMaximize:
       for n in options['nconfig']:
         for jast in options['jastrow']:
           for opt in options['optimizer']:
-            kname=self.make_kname(k)
+            kname="qw_%i"%k
             basename=self.gen_basename(k,n,jast,opt)
             f = open(basename+".max",'w')
             f.write(self.maximizeinput(k,n,jast,opt))
@@ -621,13 +621,13 @@ class QWalkRunMaximize:
       "NCONFIG %i "%n+
       "}"
       ]
-    outlist.append("include %s.sys"%self.make_kname(k))
+    outlist.append("include qw_%i.sys"%k)
 
     if jast=="none" or jast=='':
-      outlist.append("trialfunc { include %s.slater }"%self.make_kname(k))
+      outlist.append("trialfunc { include qw_%i.slater }"%k)
     else:
       opt_trans={"energy":"enopt","variance":"opt"}
-      outlist.append("trialfunc { include %s.wfout }"%self.gen_basename(k,n,jast,opt))
+      outlist.append("trialfunc { include qw_%i.%s.%s.wfout }"%(k,jast,opt_trans[opt] ))
 
     outstr = '\n'.join(outlist)
     return outstr
