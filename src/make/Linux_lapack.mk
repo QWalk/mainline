170c7a0f8c2de0cf2211c7bc81f4972b373da243
Lucas K. Wagner <lucas.wagner@gmail.com>
2016-07-12 09:12:48 -0500

    Updated Linux makefile.

--- src/make/Linux_lapack.mk
+++ src/make/Linux_lapack.mk
@@ -1,26 +1,20 @@
 ######################################################################
 # Compiler definitions for Linux systems
-#  all compiler specific information should be declared here
-
+# Setup should work for Ubuntu systems with ATLAS and OpenMPI installed
 CXX:=mpicxx
-CXX_SERIAL:=mpicxx
-include $(HOME)/mylibs/make.inc
-CXX:=mpicxx -I/usr/include/x86_64-linux-gnu/c++/4.8/ -I$(HOME)/mylibs/include
+CXX_SERIAL:=g++
 
-CXXFLAGS := -O3  \
-   -funroll-loops -ffast-math \
-   -fomit-frame-pointer
+CXXFLAGS := -O2  
 CXXFLAGS += -DUSE_MPI -DUSE_BLAS -DUSE_LAPACK  ${INCLUDEPATH}
 
-LAPACK_LIBS := -L/usr/lib/atlas-base -llapack_atlas -lf77blas -lcblas -latlas
-LAPACK_LIBS += -L/usr/lib/atlas-base/atlas/ -llapack -lblas
-LAPACK_INCLUDE := -I/usr/include/atlas
+LAPACK_LIBS := -L/usr/lib/atlas-base -llapack -lf77blas -lcblas -latlas -lf77blas 
+LAPACK_INCLUDE := -I/usr/include/atlas/
+
 BLAS_LIBS :=  $(LAPACK_LIBS) 
-#BLAS_LIBS +=  -L${HOME}/mylibs -cblas
 BLAS_INCLUDE := $(LAPACK_INCLUDE) 
 
 DEBUG:= -Wall -DNO_RANGE_CHECKING -DNDEBUG   -D__USE_GNU -DDEBUG_WRITE
-#LDFLAGS:= -static
+LDFLAGS:= 
 
 ######################################################################
 # This is the invokation to generate dependencies
