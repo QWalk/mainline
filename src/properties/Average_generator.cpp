919f737da9a27cb24f771c9c9dcf65abff051037
Lucas K. Wagner <lucas.wagner@gmail.com>
2017-09-09 10:06:05 -0500

    Added magsk, which computes the magnetic structure factor.

--- src/properties/Average_generator.cpp
+++ src/properties/Average_generator.cpp
@@ -18,6 +18,9 @@ int decide_averager(string & label, Average_generator *& avg) {
     avg=new Average_dipole;
   else if(caseless_eq(label,"SK")) 
     avg=new Average_structure_factor;
+  else if(caseless_eq(label,"MAGSK")) 
+    avg=new Average_magnetic_structure_factor;
+  
   else if(caseless_eq(label, "GR"))
     avg=new Average_twobody_correlation;
   else if(caseless_eq(label, "MANYBODY_POL"))
@@ -324,6 +327,155 @@ void Average_structure_factor::jsonOutput(Average_return & avg, Average_return &
 }
 
 //############################################################################
+
+void Average_magnetic_structure_factor::read(System * sys, Wavefunction_data * wfdata, vector <string> & words) {
+  unsigned int pos=0;
+  int np_side;
+  if(!readvalue(words, pos=0, np_side, "NGRID")) 
+    np_side=5;
+  
+  Array2 <doublevar> gvec;
+  vector <string> gvec_sec;
+  if(readsection(words, pos=0, gvec_sec, "GVEC")) {
+    int count=0;
+    gvec.Resize(3,3);
+    for(int i=0; i< 3; i++) { 
+      for(int j=0; j< 3; j++) { 
+        gvec(i,j)=atof(gvec_sec[count++].c_str());
+      }
+    }
+  }
+  else { 
+    if(!sys->getRecipLattice(gvec)) 
+      error("You don't have a periodic cell and you haven't specified GVEC for S(k)");
+  }
+  
+  int direction;
+  if(readvalue(words, pos=0, direction, "DIRECTION")) { 
+    npoints=np_side;
+    kpts.Resize(npoints, 3);
+    kpts=0;
+    for(int ix=0; ix < np_side; ix++) { 
+      for(int i=0; i< 3; i++) 
+        kpts(ix,i)=2*pi*gvec(direction,i)*ix;
+    }
+  }
+  else { 
+    npoints=np_side*np_side*np_side; 
+
+    kpts.Resize(npoints, 3);
+    kpts=0;
+    int c=0;
+    for(int ix=0; ix < np_side; ix++) {
+      for(int iy=0; iy < np_side; iy++) {
+        for(int iz=0; iz < np_side; iz++) {
+          for(int i=0; i< 3; i++)
+            kpts(c,i)+=2*pi*(gvec(0,i)*ix+gvec(1,i)*iy+gvec(2,i)*iz);
+          c++;
+        }
+      }
+    }
+  }
+}  
+
+//-----------------------------------------------------------------------------
+
+void Average_magnetic_structure_factor::evaluate(Wavefunction_data * wfdata, Wavefunction * wf,
+                                        System * sys, Sample_point * sample, Average_return & avg) {
+  avg.type="magsk";
+  int nelectrons=sample->electronSize();
+  
+  Array1 <doublevar> pos1(3),pos2(3);
+  avg.vals.Resize(npoints*2);
+  avg.vals=0;
+  int nup=sys->nelectrons(0);
+  for(int p=0; p < npoints; p++) { 
+    doublevar sum_cos=0, sum_sin=0;
+    
+    for(int e1=0; e1< nelectrons; e1++) {
+      sample->getElectronPos(e1,pos1);
+      int s1=e1 < nup?1:-1;
+      for(int e2=0; e2 < nelectrons; e2++) {
+        sample->getElectronPos(e2,pos2);
+        doublevar dot=0;
+        for(int d=0; d< 3; d++) dot+=(pos1(d)-pos2(d))*kpts(p,d);
+        int s2=e2 < nup?1:-1;
+        sum_cos+=cos(dot)*s1*s2;
+        sum_sin+=sin(dot)*s1*s2;
+      }
+    }
+    avg.vals(2*p)+=sum_cos;
+    avg.vals(2*p+1)+=sum_sin;
+    
+  }
+  
+}
+
+//-----------------------------------------------------------------------------
+
+void Average_magnetic_structure_factor::write_init(string & indent, ostream & os) { 
+  os << indent << "magsk\n";
+  for(int i=0; i< npoints; i++) { 
+    os << indent << "kpoint { " << kpts(i,0) << " " << kpts(i,1) << " " 
+    << kpts(i,2) << " } " <<endl;
+  }
+}
+//-----------------------------------------------------------------------------
+
+void Average_magnetic_structure_factor::read(vector <string> & words) { 
+  vector <vector <string> > kpttext;
+  vector <string> tmp;
+  unsigned int pos=0;
+  while(readsection(words, pos, tmp, "KPOINT")) kpttext.push_back(tmp);
+  npoints=kpttext.size();
+  kpts.Resize(npoints,3);
+  for(int i=0; i< npoints; i++) { 
+    for(int d=0; d< 3; d++) { 
+      kpts(i,d)=atof(kpttext[i][d].c_str());
+    }
+  }
+}
+//-----------------------------------------------------------------------------
+
+void Average_magnetic_structure_factor::write_summary(Average_return & avg, Average_return & err, ostream & os) { 
+  os << "Structure factor \n";
+  os << "    k  s(k) s(k)err  kx  ky  kz" << endl;
+  assert(avg.vals.GetDim(0) >=2*npoints);
+  assert(err.vals.GetDim(0) >=2*npoints);
+  
+  for(int i=0; i< npoints; i++) {
+    doublevar skr=avg.vals(2*i), ski=err.vals(2*i+1);
+    doublevar skrerr=err.vals(2*i),skierr=err.vals(2*i+1);
+    doublevar r=0;
+    for(int d=0; d< 3; d++) r+=kpts(i,d)*kpts(i,d);
+    r=sqrt(r);
+    os << "sk_out " <<  r << "   " << skr << "   " << skrerr 
+       << "   " << ski << "   " << skierr 
+      << "  " << kpts(i,0) << "   " << kpts(i,1) << "   " << kpts(i,2) << endl;
+  }
+  
+}
+
+//--------------------------------------------------------------------------------
+void Average_magnetic_structure_factor::jsonOutput(Average_return & avg, Average_return & err, ostream & os) {
+    
+  assert(avg.vals.GetDim(0) >=npoints);
+  assert(err.vals.GetDim(0) >=npoints);
+    
+  os << "\"" << avg.type << "\":{" << endl;
+  os << "\"k\":";
+  jsonarray(os,kpts);
+  os << ",\n";
+
+  os << "\"value\":";jsonarray(os,avg.vals);
+  os << ",\n";
+  os << "\"error\":"; jsonarray(os,err.vals); 
+    
+  os << "}" << endl;
+}
+
+//############################################################################
+
 
 void Average_fourier_density::read(System * sys, Wavefunction_data * wfdata, vector <string> & words) {
   unsigned int pos=0;
--- src/properties/Average_generator.h
+++ src/properties/Average_generator.h
@@ -144,6 +144,24 @@ private:
 };
 
 //----------------------------------------------------------------------------
+class Average_magnetic_structure_factor:public Average_generator { 
+public:
+  virtual void evaluate(Wavefunction_data * wfdata, Wavefunction * wf,
+                        System * sys, Sample_point * sample, Average_return & );
+  virtual void read(System * sys, Wavefunction_data * wfdata, vector
+                    <string> & words);
+  virtual void write_init(string & indent, ostream & os);
+  virtual void read(vector <string> & words);
+  virtual void write_summary(Average_return &,Average_return &, ostream & os);
+  virtual void jsonOutput(Average_return &,Average_return &, ostream & os);
+private:
+  int npoints;
+  Array2 <doublevar> kpts;
+  
+};
+
+//----------------------------------------------------------------------------
+
 class Average_fourier_density:public Average_generator { 
 public:
   virtual void evaluate(Wavefunction_data * wfdata, Wavefunction * wf,
