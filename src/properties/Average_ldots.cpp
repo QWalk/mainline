5ee86b46ff5d3995aa2a0e2529d31e20bd91a66f
YueqingChang <yueqing2@illinois.edu>
2018-01-19 09:16:50 -0600

    Rewrite the L dot S generator

--- src/properties/Average_ldots.cpp
+++ src/properties/Average_ldots.cpp
@@ -9,40 +9,43 @@ void Average_ldots::read(vector <string> & words){
 }
 
 void Average_ldots::write_init(string & indent, ostream & os){
-  os << indent << "L dot S\n";
+  os << indent << "LdotS\n";
 }
 
 void Average_ldots::evaluate(Wavefunction_data * wfdata, Wavefunction * wf, System * sys,
                           Sample_point * sample, Average_return & avg) {
   avg.type="ldots";
   int ndim=3;
- // int nwf=wf->nfunc();
-  avg.vals.Resize(1);
+  int nwf=wf->nfunc();
+  avg.vals.Resize(nwf*2);
   avg.vals=0.0;
   int nelectrons=sample->electronSize();
   int nions=sample->ionSize();
   Array1 <doublevar> e_pos(ndim);    
   Array1 <doublevar> ion_pos(ndim);
-  //get the gradient of wavefunction
-  Parm_deriv_return derivatives;
-  derivatives.need_hessian = 0;
-  wf->updateVal(wfdata, sample);
-  if(!wfdata->supports(parameter_derivatives))
-    error("Wavefunction needs to supports analytic parameter derivatives");
- // if(!wf->getParmDeriv(wfdata, sample,derivatives)) {
- //   error("WF needs to support parmderivs for now.");
-//  }
-  wf->getParmDeriv(wfdata, sample, derivatives);  
-  for(int e=0; e< nelectrons; e++) {
-    int spin = 1;
-    if(e >= sys->nelectrons(0)) spin = -1;
-    sample->getElectronPos(e,e_pos);
-    cout << "get here!!" << endl;
-    for(int at=0; at < nions; at++) {
-      sample->getIonPos(at,ion_pos);
-      avg.vals(0) += spin*(e_pos(0)-ion_pos(0))*derivatives.val_gradient(e,1) - spin*(e_pos(1)-ion_pos(1))*derivatives.val_gradient(e,0); 
-    }  
-  }    
+  int totalspin;
+  totalspin = sys->nelectrons(0) - sys->nelectrons(1);
+  
+  Wf_return temp_lap;
+  temp_lap.Resize(nwf,5);
+  //wf->updateVal(wfdata, sample);
+
+  for(int i=0; i< nwf; i++){
+    for(int e=0; e< nelectrons; e++) {
+      //int spin = 1;
+      //if(e >= sys->nelectrons(0)) spin = -1;
+      sample->getElectronPos(e,e_pos);
+      for(int at=0; at < nions; at++) {
+        sample->getIonPos(at,ion_pos);
+        wf->getLap(wfdata, e, temp_lap);  
+      //  avg.vals(2*i) += totalspin*(e_pos(0)-ion_pos(0))*temp_lap.amp(i,2)*cos(temp_lap.phase(i,2)) - totalspin*(e_pos(1)-ion_pos(1))*temp_lap.amp(i,1)*cos(temp_lap.phase(i,1));
+        avg.vals(2*i) += (e_pos(0)-ion_pos(0))*temp_lap.amp(i,2)*cos(temp_lap.phase(i,2)) - (e_pos(1)-ion_pos(1))*temp_lap.amp(i,1)*cos(temp_lap.phase(i,1));
+      //  avg.vals(2*i+1) += totalspin*(e_pos(0)-ion_pos(0))*temp_lap.amp(i,2)*sin(temp_lap.phase(i,2)) -totalspin*(e_pos(1)-ion_pos(1))*temp_lap.amp(i,1)*sin(temp_lap.phase(i,1));
+        avg.vals(2*i+1) += (e_pos(0)-ion_pos(0))*temp_lap.amp(i,2)*sin(temp_lap.phase(i,2)) - (e_pos(1)-ion_pos(1))*temp_lap.amp(i,1)*sin(temp_lap.phase(i,1));
+      }  
+    }
+  }
+
 }
 
 void Average_ldots::write_summary(Average_return & avg, Average_return & err,
